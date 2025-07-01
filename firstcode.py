63d15ab23852bb5ab9e59fd269d866c2ded51322
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-07-02 01:20:06 +0500

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,41 @@
+# # print("hello python")
+# a=3 
+# b=4
+# # print(a+b)
+# print('''TWINKLE, TWINKLE, LITTLE STAR
+# Twinkle, twinkle, little star, how I wonder what you are. Up above the world so high,
+# like a diamond in the sky. Twinkle, twinkle, little star, how I wonder what you are.
+# When the blazing sun is set, and the grass with dew is wet. Then you show your little
+# light, twinkle, twinkle all the night. Twinkle, twinkle little star, how I wonder what you
+# are.
+# Then the traveler in the dark thanks you for your tiny spark. How could he see where to
+# go if you did not twinkle so? Twinkle, twinkle little star, how I wonder what you are.
+# As your bright and tiny spark lights the traveler in the dark, though I know not what you
+# are, twinkle, twinkle, little star. Twinkle, twinkle, little star, how I wonder what you are.''')
+# a=4
+# a+=5
+# # print(a)
+# a=5
+# print(a)
+# a=5.66
+# print(type(a))
+
+# a="hello"
+# print(type(a))
+
+# a=True
+# print(type(a))
+
+# a=None
+# print(type(a))
+
+a=int (input("Enter a number: "))
+b=int (input("Enter a number: "))
+print("number 1 is: ", a)
+print("number 2 is: ", b)
+print("sum is: ", a+b)
+print("difference is: ", a-b)
+print("product is: ", a*b)
+print("division is: ", a/b)
+
+print("floor division is: ", a//b)
---
+++ index.py
@@ -0,0 +1,20 @@
+# a=int(input("enter your age:"))
+
+# if(a>18):
+#     print("you can vote")
+
+# else:
+#     print("you cant vote")
+# a=int(input("enter your age:"))
+# if(a%2==0):
+#     print("a is even")
+# else:
+#     print("a isnot  even")
+marks1=int(input("enter marks1:"))
+marks2=int(input("enter marks2:"))
+marks3=int(input("enter marks3:"))
+total_percentage=100* (marks1+marks2+marks3)/100
+if (total_percentage>=40):
+    print("you are passes")
+else:
+   print("you are fail")
