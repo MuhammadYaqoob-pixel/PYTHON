f54008da3621d081a36fd0624b7b90dea22fc734
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-07-11 22:04:50 +0500

    Add files via upload

--- index.py
+++ index.py
@@ -1,87 +1,39 @@
-# for i in range(1, 6):
-#     print(i)
+# def avg():
+#     a=int ( input("enter a "))
+#     b=int ( input("enter b "))
+#     c=int ( input("enter c "))
+#     average=(a+b+c)/3
+#     print(average)
+
+# avg()
+
+# def goodday():
+#     a=int(input("enter value of a"))
+#     b=int(input("enter value of b"))
+#     print(a+b)   
+
+# goodday()
+# goodday()
+# print("thank you for using fuctions")
 
 
-
-
-# i=1
-# while(i<7):
-#     print(i)
-#     i+=1
-
-
-
-# l=[1, 3, "yaqoob", "hajjan"]
-# i=0
-
-
-# while(i<len(l)):
-
-#       i+=1
-# print(l)
-
-
-# l=[22, 44, 44,77,88]
-
-# for i in  l:
-#    print(i)
-
-# for i in range (100):
-#     if(i==6):
-#         break;
-#     print(i)
-
-
-
-# for i in range (100):
-#     if(i==6):
-#         continue;
-#     print(i)
-
-
-# n=int (input("enter any number:"))
-
-# for i in range(1,11):
-#     print(f"{n}*{i}={n*i}")
+# def goodday(name):
+#     print("soory you are fail," + name)
     
-    #print(i)
+# goodday("hajjan") 
 
-
-# l=["yaqoob", "yasir ", "hajjan", "zameer"]
-
-# for name in l:
-#     if(name.startswith ("y")):print(f"hello {name}")
-
-
-# n=int (input("enter any number:"))
-# i=1
-# while(i<11):
-#     print(f"{n}*{i}={n*i}")
-#     i+=1
+# goodday("yaqoob")
+# goodday("janii")
 
 
 
-# n=int (input("enter any number:"))
 
-# for i in range(2, n):
-#     if(n%i)==0:
-#         print("number is not prime")
-#         break
-#     else:
-# #         print("number is prime")
-# n=int (input("enter any number:"))
-# product=1
+def factorial():
+    if(n==1 or n==0):
+        return 1
+    
+    return n*factorial(n-1)
+n=int(input("enter a number:"))
 
-# for i in range(1, n+1):
+print(f"the factorial is :{factorial (n)}")
 
-#     product= product* i
-
-#     print(f"the factorail of{n} is {product}")
-
-
-n=int (input("enter any number:"))
-
-for i in range(1, n+1):
-    print(" "*(n-1), end="")
-    print("*"* (2*i-1), end ="")
-    print("")
