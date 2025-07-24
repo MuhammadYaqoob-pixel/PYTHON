40790f5956256c0b7645154b4d7c9b5996822261
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-07-24 11:14:25 +0500

    Add files via upload
    basics oflist

---
+++ list.py
@@ -0,0 +1,54 @@
+# list=["apple","banana", 5, 6,7,"orange"]
+
+# # print(list)
+
+# #print(type(list))
+
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+
+# print(list[4])
+
+fruits=["apple", "orange", "pineapple", 5,6,7]
+# fruits[0]=3
+# print(fruits)
+
+# fruits[3]="seaborn"
+# print(fruits)
+
+
+# fruits.append("grapes")
+
+# print(fruits)
+
+# fruits.insert(1, "apple")
+
+# print(fruits)
+
+# fruits.remove("apple")
+# print(fruits)
+
+# fruits.pop()
+# print(fruits)
+
+# fruits.clear()
+# print(fruits)
+
+# fruits.extend(["apple", "orange", "pineapple", 5,67,8])
+# print(fruits)
+
+# LIST SLICING
+# list=[1,2,3,4,5,6,7,8,9,0]
+# print(list[0:3])
+
+# print(list[3:6])
+
+# print(list[:8])
+
+numbers = [10, 5, 8]
+print(len(numbers))   # 3
+print(max(numbers))   # 10
+print(min(numbers))   # 5
+print(sum(numbers))   # 23
