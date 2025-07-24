64c70cbb92328c8cec74f769f9d24372f530caf2
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-07-24 10:33:07 +0500

    Add files via upload
    here i am practice multiple programs in python

---
+++ Practice.py
@@ -0,0 +1,69 @@
+# a="hello sir, how are you"
+
+# print(a.upper())
+# print(a.lower())
+# print(f"{a}, i am muhammad yaqoob")
+
+
+# marks=int(input("plz enter your marks:"))
+# # marks=40
+
+# if marks>80:
+
+#     print("A GRADE")
+# elif marks>70:
+
+#     print("B GRADE")
+# elif marks>60:
+
+#     print(" C GRADE")
+
+# else:
+
+#     print(" sorry sir you are fail try again")
+
+
+
+# for i in range(7):
+#  print(i)
+
+
+# sqaure print starts
+# rows=6
+# for i in range (rows):
+#     print("*"*rows)
+
+# rectangle pattern
+# rows=4
+# cols=7
+# for i in range(rows):
+#     print("*"*cols)
+
+
+
+# Right-Angle Triangle
+
+# x=5
+# for i in range(1,x+1):
+#  print("*"*i)
+# y=7
+# for i in range(1, y+1):
+#   print("*"*i)
+
+
+
+#    Inverted Right-Angle Triangle
+# python
+# Copy
+# Edit
+
+# x=6
+# for i in range(x, 0,-1):
+#     print("*"*i)
+
+# Pyramid (Triangle)
+
+# y=6
+# for i in range (1, y+1):
+#       print(" " * ( y-i)+ "*" *i)
+
