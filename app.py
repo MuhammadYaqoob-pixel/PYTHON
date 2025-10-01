93065dc76b7f024d0411ff241969662c84be81dd
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-10-01 12:39:18 +0500

    Add files via upload
    working by using pandas labrary of python where we easily clean and restore data and this labrary is very efficient library of python in the field of data analytics

---
+++ app.py
@@ -0,0 +1,4 @@
+# print("hello world")
+import pandas as pd
+df=pd.read_csv("sales_data_sample.csv", encoding="latin1")
+print(df)
---
+++ data.py
@@ -0,0 +1,25 @@
+# Importing pandas
+import pandas as pd
+
+# Data in dictionary format
+data = {
+    "Name": ["Ali", "Sara", "Ahmed", "Zainab", "Usman", "Ayesha", "Bilal", "Hira", "Imran", "Nida",
+             "Kashif", "Maha", "Tariq", "Fatima", "Danish", "Sana", "Junaid", "Mehwish", "Rizwan", "Laiba"],
+    "Age": [25, 28, 32, 23, 29, 27, 35, 26, 30, 24,
+            31, 22, 36, 27, 33, 25, 29, 26, 34, 23],
+    "Salary": [45000, 55000, 60000, 40000, 48000, 52000, 70000, 46000, 50000, 42000,
+               61000, 39000, 75000, 53000, 67000, 47000, 59000, 49000, 72000, 41000],
+    "City": ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Multan", "Faisalabad", "Hyderabad",
+             "Sialkot", "Sukkur", "Rawalpindi", "Gujranwala", "Bahawalpur", "Sahiwal", "Okara", "Abbottabad",
+             "Mirpur", "Gilgit", "Skardu", "Muzaffarabad"]
+}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Display the DataFrame
+print(df)
+df["bonus"]=df['Salary']*0.1
+print(df)
+df.insert(0, "employeeid",[ 10,20,30,40, 50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200])
+print(df)
